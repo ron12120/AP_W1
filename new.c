ddb6e499711642fd16cd93e25ef80671b58ef88f
ron12120 <76705730+ron12120@users.noreply.github.com>
2024-06-03 18:32:06 +0300

    Update new.c

--- new.c
+++ new.c
@@ -11,66 +11,48 @@
 #include <termios.h>
 #include <unistd.h>
 
-char command[1024], lastCommand[1024], currentCommand[1024], prompt[1024];
-int last_command, fd, amper, rv, piping, i;
-int Duplicate_FD, mainProcess, status = 0;
-int change_status(char **args);
-char current_command[1024];
-List variables, commands;
-char **CountPIPEPointer;
-pid_t ProccesID, cpid;
-char *end_if = "fi\n";
-char *new_command2;
-char *new_command;
-char *argv[1024];
-char *outfile;
-int pipeFD[2];
-char *token;
-int enter = 0;
+#define MAX_COMMAND 1024
+
+char command[MAX_COMMAND], last_command[MAX_COMMAND], current_command[MAX_COMMAND], prompt[MAX_COMMAND], current_command[MAX_COMMAND];
+int command_counter, file_desc, amp, rv, piping, i, duplicate_fd, main_process, status = 0, pipe_fd[2], enter = 0;
+List vars, comms;
+pid_t pid, cpid;
+char* end_if = "fi\n", *new_command, *new_command2, *output_file, *token;
+char *argv[MAX_COMMAND];
 char a, b, c;
 
-char **CountPIPE(char **args)
-{
-    char **CounterPIPE = args;
-    while (*CounterPIPE != NULL)
-    {
-        if (strcmp(*CounterPIPE, "|") == 0)
-        {
-            return CounterPIPE;
-        }
+int change_status(char **args);
 
-        CounterPIPE++;
-    }
-    return NULL;
-}
-
-int CountARGS(char **args)
+int count_args(char **args)
 {
-    char **CounterARGS = args;
+    char **counter_args = args;
     int cnt = 0;
-    while (*CounterARGS != NULL)
+    for (cnt; *counter_args != NULL; cnt++)
     {
-        CounterARGS++;
-        cnt++;
+        counter_args++;
     }
     return cnt;
 }
 
-char *safe_strcpy(char *dest, size_t size, char *src)
+char **count_pipes(char **args)
 {
-    if (size > 0)
+    char **counter_pipes = args;
+    while (*counter_pipes != NULL)
     {
-        *dest = '\0';
-        return strncat(dest, src, size - 1);
+        if (strcmp(*counter_pipes, "|") == 0)
+        {
+            return counter_pipes;
+        }
+
+        counter_pipes++;
     }
-    return dest;
+    return NULL;
 }
 
 int first_index_in_str(char *arr)
 {
-
     int j;
-    for (j = 0; i < 1024; i++)
+    for (j = 0; i < MAX_COMMAND; i++)
     {
         if (strlen(arr) <= i)
         {
@@ -80,9 +62,36 @@ int first_index_in_str(char *arr)
     return j;
 }
 
-void split(char *command)
+char *safe_strcpy(char *dst, size_t size, char *src)
 {
+    if (size > 0)
+    {
+        *dst = '\0';
+        return strncat(dst, src, size - 1);
+    }
+    return dst;
+}
 
+void termination_handler(int signum)
+{
+    if (getpid() == main_process)
+    {
+        printf("\n");
+        printf("You typed Control-C!");
+        printf("\n");
+        write(0, prompt, strlen(prompt) + 1);
+        return;
+    }
+    else
+    {
+        fprintf(stderr, "\ncaught signal: %d\n", signum);
+        kill(main_process, SIGKILL);
+        return;
+    }
+}
+
+void split_command(char *command)
+{
     char *part_of_command = strtok(command, " ");
     int i = 0;
     while (part_of_command != NULL)
@@ -93,37 +102,18 @@ void split(char *command)
     }
     argv[i] = NULL;
 }
-// Task number: 8
-void termination_handler(int signum)
-{
-    if (getpid() == mainProcess)
-    {
-        printf("\nYou typed Control-C!\n");
-        write(0, prompt, strlen(prompt) + 1);
-        return;
-    }
-    else
-    {
-        fprintf(stderr, "\ncaught signal: %d\n", signum);
-        kill(mainProcess, SIGKILL);
-        return;
-    }
-}
 
-int execute(char **args)
+int run_execute(char **args)
 {
-
     rv = -1;
     piping = 0;
-    i = CountARGS(args);
-    char **CountPIPEPointer = CountPIPE(args);
-    // Task number: 9
-
-    if (CountPIPEPointer != NULL)
+    i = count_args(args);
+    char **count_pipes_pointer = count_pipes(args);
+    if (count_pipes_pointer != NULL)
     {
-        *CountPIPEPointer = NULL;
+        *count_pipes_pointer = NULL;
         piping++;
-        int result = pipe(pipeFD);
+        int result = pipe(pipe_fd);
         if (result == -1)
         {
             printf("Failed to create pipe\n");
@@ -137,7 +127,7 @@ int execute(char **args)
         }
         else if (cpid == 0)
         {
-            int status = close(pipeFD[1]);
+            int status = close(pipe_fd[1]);
             if (status == -1)
             {
                 printf("Failed to close file descriptor\n");
@@ -149,79 +139,68 @@ int execute(char **args)
                 printf("Failed to close standard input file descriptor\n");
                 exit(EXIT_FAILURE);
             }
-            int newFD = dup(pipeFD[0]);
-            if (newFD == -1)
+            int new_fd = dup(pipe_fd[0]);
+            if (new_fd == -1)
             {
                 printf("Failed to duplicate file descriptor\n");
                 exit(EXIT_FAILURE);
             }
-            execute(CountPIPEPointer + 1);
+            run_execute(count_pipes_pointer + 1);
             exit(0);
         }
-
-        Duplicate_FD = dup(STDOUT_FILENO);
-        if (Duplicate_FD == -1)
+        duplicate_fd = dup(STDOUT_FILENO);
+        if (duplicate_fd == -1)
         {
             printf("Failed to duplicate file descriptor\n");
             exit(EXIT_FAILURE);
         }
-        dup2(pipeFD[1], STDOUT_FILENO);
+        dup2(pipe_fd[1], STDOUT_FILENO);
     }
-
     if (args[0] == NULL)
         return 0;
-
     if (!strcmp(args[i - 1], "&"))
     {
-        amper = 1;
+        amp = 1;
         args[i - 1] = NULL;
     }
-
     int redirect = -1;
-    // Task number: 1
     if (i >= 2 && (!strcmp(argv[i - 2], ">") || !strcmp(argv[i - 2], ">>")))
     {
-        outfile = argv[i - 1];
+        output_file = argv[i - 1];
         redirect = 1;
     }
     else if (i >= 2 && !strcmp(argv[i - 2], "2>"))
     {
-        outfile = argv[i - 1];
+        output_file = argv[i - 1];
         redirect = 2;
     }
     else if (i >= 2 && !strcmp(argv[i - 2], "<"))
     {
-        outfile = argv[i - 1];
+        output_file = argv[i - 1];
         redirect = 0;
     }
-
-    // Task number: 2
     if (strcmp(args[0], "prompt") == 0)
     {
         if (!strcmp(argv[1], "="))
         {
             strcpy(prompt, args[2]);
+            strcat(prompt, ": ");
         }
-
         return 0;
     }
     if (strcmp(args[0], "echo") == 0)
     {
         char **echo_var = args + 1;
-        // Task number: 4
         if (strcmp(*echo_var, "$?") == 0)
         {
             printf("%d\n", status);
             return 0;
         }
-
         while (*echo_var)
         {
-
             if (echo_var != NULL && echo_var[0][0] == '$')
             {
-                // Task number: 3
-                Node *node = variables.head;
+                Node *node = vars.head;
                 char *new_variable = NULL;
                 while (node)
                 {
@@ -234,23 +213,17 @@ int execute(char **args)
                 if (new_variable != NULL)
                     printf("%s ", new_variable);
             }
-
             else
             {
-                // Task number: 3
                 printf("%s ", echo_var[0]);
             }
-
             echo_var++;
         }
         printf("\n");
         return 0;
     }
-
     else
-        amper = 0;
-
-    // Task number: 5
+        amp = 0;
     if (strcmp(args[0], "cd") == 0)
     {
         if (chdir(args[1]) != 0)
@@ -259,23 +232,20 @@ int execute(char **args)
         }
         return 0;
     }
-    // Task number:  6
     if (strcmp(args[0], "!!") == 0)
     {
-        if (commands.size > 0)
+        if (comms.size > 0)
         {
-            strcpy(currentCommand, lastCommand);
-            split(currentCommand);
-            execute(argv);
+            strcpy(current_command, last_command);
+            split_command(current_command);
+            run_execute(argv);
         }
         else
         {
             printf("The command history list is empty");
         }
-
         return 0;
     }
-    // Task number: 10
     if (args[0][0] == '$' && i > 2)
     {
         Var *var = (Var *)malloc(sizeof(Var));
@@ -283,44 +253,39 @@ int execute(char **args)
         var->value = malloc((strlen(args[2]) + 1));
         strcpy(var->key, args[0]);
         strcpy(var->value, args[2]);
-        add(&variables, var);
+        add(&vars, var);
         return 0;
     }
-    // Task number: 11
     if (strcmp(args[0], "read") == 0)
     {
         Var *var = (Var *)malloc(sizeof(Var));
         var->key = malloc(sizeof(char) * (strlen(args[1])));
-        var->value = malloc(sizeof(char) * 1024);
+        var->value = malloc(sizeof(char) * MAX_COMMAND);
         var->key[0] = '$';
-        memset(var->value, 0, 1024);
+        memset(var->value, 0, MAX_COMMAND);
         strcpy(var->key + 1, args[1]);
-        fgets(var->value, 1024, stdin);
+        fgets(var->value, MAX_COMMAND, stdin);
         var->value[strlen(var->value) - 1] = '\0';
-        add(&variables, var);
+        add(&vars, var);
         return 0;
     }
-
-    ProccesID = fork();
-
-    // Continue task number: 1
-    if (ProccesID == 0)
+    pid = fork();
+    if (pid == 0)
     {
-
         if (redirect >= 0)
         {
             if (!strcmp(args[i - 2], ">>"))
             {
-                if ((fd = open(outfile, O_APPEND | O_CREAT | O_WRONLY, 0660)) < 0)
+                if ((file_desc = open(output_file, O_APPEND | O_CREAT | O_WRONLY, 0660)) < 0)
                 {
                     perror("Error: Can't create file");
                     exit(1);
                 }
-                lseek(fd, 0, SEEK_END);
+                lseek(file_desc, 0, SEEK_END);
             }
             else if (!strcmp(args[i - 2], ">") || !strcmp(args[i - 2], "2>"))
             {
-                if ((fd = creat(outfile, 0660)) < 0)
+                if ((file_desc = creat(output_file, 0660)) < 0)
                 {
                     perror("Error: Can't create file");
                     exit(1);
@@ -328,11 +293,11 @@ int execute(char **args)
             }
             else
             {
-                fd = open(outfile, O_RDONLY);
+                file_desc = open(output_file, O_RDONLY);
             }
             close(redirect);
-            dup(fd);
-            close(fd);
+            dup(file_desc);
+            close(file_desc);
             args[i - 2] = NULL;
         }
         int status_code = execvp(args[0], args);
@@ -342,18 +307,17 @@ int execute(char **args)
             exit(1);
         }
     }
-    else if (ProccesID == -1)
+    else if (pid == -1)
     {
         printf("Error forking a new process\n");
         exit(1);
     }
     else
     {
-        ProccesID = -1;
+        pid = -1;
         wait(&status);
         rv = status;
     }
-
     if (piping != 0)
     {
         int status = close(STDOUT_FILENO);
@@ -362,16 +326,15 @@ int execute(char **args)
             printf("Failed to close standard output file descriptor\n");
             exit(EXIT_FAILURE);
         }
-        int newFD = dup(Duplicate_FD);
+        int newFD = dup(duplicate_fd);
         if (newFD == -1)
         {
             printf("Failed to duplicate file descriptor\n");
             exit(EXIT_FAILURE);
         }
-        int status_pipeFD = close(pipeFD[1]);
+        int status_pipeFD = close(pipe_fd[1]);
         if (status_pipeFD == -1)
         {
-
             printf("Failed to close file descriptor\n");
             exit(EXIT_FAILURE);
         }
@@ -390,27 +353,27 @@ int change_status(char **args)
     }
     else
     {
-        rv = execute(args);
+        rv = run_execute(args);
         return rv;
     }
 }
 
 int main()
 {
-    mainProcess = getpid();
+    main_process = getpid();
     signal(SIGINT, termination_handler);
     strcpy(prompt, "hello: ");
-    last_command = 0;
-    char *prevCommand = malloc(sizeof(char) * 1024);
+    command_counter = 0;
+    char *prev_command = malloc(sizeof(char) * MAX_COMMAND);
     struct termios old_terminal, new_terminal;
     tcgetattr(STDIN_FILENO, &old_terminal);
-
     while (1)
     {
         memset(command, 0, sizeof(command));
         new_terminal = old_terminal;
         new_terminal.c_lflag &= (ECHOE | ~ICANON);
         tcsetattr(STDIN_FILENO, TCSANOW, &new_terminal);
+        printf("\r");
         printf("%s", prompt);
         i = 1;
         c = getchar();
@@ -418,47 +381,46 @@ int main()
         {
             if (i < strlen(prompt) + 1)
             {
-                printf("\b\b\b   \b");
+                printf("\b\b\b\b");
             }
             continue;
         }
         else if (c == '\033')
         {
-            // Task number: 12
             printf("\33[2K");
             a = getchar();
             b = getchar();
             switch (b)
             {
             case 'A':
-                if (commands.size == 0)
+                if (comms.size == 0)
                 {
                     break;
                 }
-                else if (last_command > 0)
+                else if (command_counter > 0)
                 {
-                    last_command--;
+                    command_counter--;
                 }
                 printf("\b\b\b\b");
-                prevCommand = (char *)get_command(&commands, last_command);
-                printf("%s", (char *)get_command(&commands, last_command));
+                prev_command = (char *)get_command(&comms, command_counter);
+                printf("%s", (char *)get_command(&comms, command_counter));
                 break;
             case 'B':
-                if (commands.size == 0 || last_command >= commands.size - 1)
+                if (comms.size == 0 || command_counter >= comms.size - 1)
                 {
-                    if (last_command == commands.size - 1)
+                    if (command_counter == comms.size - 1)
                     {
-                        last_command++;
+                        command_counter++;
                     }
                     break;
                 }
                 else
                 {
-                    last_command++;
+                    command_counter++;
                 }
                 printf("\b\b\b\b");
-                prevCommand = (char *)get_command(&commands, last_command);
-                printf("%s", (char *)get_command(&commands, last_command));
+                prev_command = (char *)get_command(&comms, command_counter);
+                printf("%s", (char *)get_command(&comms, command_counter));
                 break;
             }
             command[0] = c;
@@ -468,16 +430,16 @@ int main()
         else if (c == '\n')
         {
             tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
-            if (commands.size > 0)
+            if (comms.size > 0)
             {
-                prevCommand = (char *)get_command(&commands, last_command);
-                new_command2 = malloc(sizeof(char) * strlen(prevCommand));
-                prevCommand[strlen(prevCommand)] = ' ';
-                strcpy(new_command2, prevCommand);
-                add(&commands, new_command2);
-                last_command = commands.size;
+                prev_command = (char *)get_command(&comms, command_counter);
+                new_command2 = malloc(sizeof(char) * strlen(prev_command));
+                prev_command[strlen(prev_command)] = ' ';
+                strcpy(new_command2, prev_command);
+                add(&comms, new_command2);
+                command_counter = comms.size;
                 strcpy(command, new_command2);
-                split(command);
+                split_command(command);
                 status = change_status(argv);
             }
             continue;
@@ -485,10 +447,9 @@ int main()
         else
         {
             tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
-            memset(command, 0, 1024);
+            memset(command, 0, MAX_COMMAND);
             command[0] = c;
             char b;
-            int Flag = 0;
             i = 1;
             while ((b = getchar()) != '\n')
             {
@@ -512,21 +473,20 @@ int main()
         int if_flag = 0;
         if (!strncmp(command, "if", 2))
         {
-            // Task number: 13
             if_flag = 1;
             while (1)
             {
-                fgets(current_command, 1024, stdin);
+                fgets(current_command, MAX_COMMAND, stdin);
                 strcat(command, current_command);
                 command[strlen(command) - 1] = '\n';
                 if (!strcmp(current_command, end_if))
                     break;
             }
-            char *commandcurr = "bash";
+            char *command_curr = "bash";
             char *argument_list[] = {"bash", "-c", command, NULL};
             if (fork() == 0)
             {
-                int status_code = execvp(commandcurr, argument_list);
+                int status_code = execvp(command_curr, argument_list);
                 printf("bash has taken control of this child process. This won't execute unless it terminates abnormally!\n");
                 if (status_code == -1)
                 {
@@ -538,25 +498,23 @@ int main()
             strcat(command, current_command);
         }
         command[strlen(command) - 1] = '\0';
+
         if (!strcmp(command, "quit"))
         {
-            // Task number: 7
             exit(0);
         }
-
         if (strcmp(command, "!!"))
         {
             tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
-            strcpy(lastCommand, command);
+            strcpy(last_command, command);
         }
-
         new_command = malloc(sizeof(char) * strlen(command));
         strcpy(new_command, command);
-        add(&commands, new_command);
-        last_command = commands.size;
+        add(&comms, new_command);
+        command_counter = comms.size;
         if (!if_flag)
         {
-            split(command);
+            split_command(command);
             status = change_status(argv);
         }
         else
